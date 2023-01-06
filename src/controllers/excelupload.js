1968dd9909df44e1ef83ffbfc3cce38b037bbf80
nerjib <kabirnajib0@gmail.com>
2023-01-06 14:43:11 +0100

    commit -m payments table

--- src/controllers/excelupload.js
+++ src/controllers/excelupload.js
@@ -136,7 +136,7 @@ router.post('/customers', async (req, res) => {
         let path = `${req.file.destination}/${req.file.originalname}`;
         readXlsxFile(path).then((rowss) => {
 
-
+          console.log('rewshhh',rowss[3]);
           if( rowss[3][1] !=='Staff ID'|| rowss[3][2] !=='Legacy Id' || rowss[3][3] !=='Full Name'|| rowss[3][4] !=='Element'|| rowss[3][5] !=='Amount'|| rowss[3][6] !=='Period'|| rowss[3][7] !=='Command' ){
             return  res.status(500).send({
               status:false,
