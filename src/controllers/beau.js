afca2edf7adbdb241e2c7c4703a3e3f3def49888
nerjib <kabirnajib0@gmail.com>
2024-02-07 14:40:39 +0100

    beuty  hub add

--- src/controllers/beau.js
+++ src/controllers/beau.js
@@ -15,7 +15,11 @@ router.get('/products', async (req, res) => {
   try {
     // const { rows } = qr.query(getAllQ);
     const { rows } = await db.query(getAllQ);
-    return res.status(201).send(rows);
+    return res.status(201).send({
+      status: true,
+      data: rows,
+      message: 'successful'
+    });
   } catch (error) {
     if (error.routine === '_bt_check_unique') {
       return res.status(400).send({ message: 'Error' });
