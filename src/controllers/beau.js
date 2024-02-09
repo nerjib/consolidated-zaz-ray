a5ebf5530f9887235837c75caa1bb33c9b9795a8
nerjib <kabirnajib0@gmail.com>
2024-02-09 10:41:09 +0100

    'stripe'

--- src/controllers/beau.js
+++ src/controllers/beau.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const moment = require ('moment')
+const stripe = require('stripe')('sk_test_51KPk4hG3qtUfMBk1g045CSDKbRInzr7aAC8pKMPzXUZzq1LJgADNGlmg1t5Odty74vcCVXScVopX5t2WAJLZfzYk00j0207GVb');
 const router = express.Router();
 const db = require('../db/index');
 const dotenv = require('dotenv');
 const upload = require('./multer')
 const cloudinary = require('./cloudinary')
-
-
 
 
 
@@ -227,6 +226,63 @@ router.get('/transactions', async (req, res) => {
     return res.status(400).send(error);
     }  
   //  },{ resource_type: "auto", public_id: `ridafycovers/${req.body.title}` })
+} else {
+    res.status(405).json({
+      err: `${req.method} method not allowed`
+    })
+  }
+
+  });
+
+  router.post('/create-checkout',   async(req, res) => {
+
+    if (req.method === 'POST') {
+    const { products } = req.body;
+
+    const lineItems = products.map((product) => ({
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: product.name,
+          images: [product.imgurl],
+          price: product.price,
+        },
+        unit_amount: product.price *100,        
+      },
+      quantity: product.qty,      
+    }));
+
+    const session = await stripe.checkout.session.create({
+        payment_method: 'card',
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: 'nerjib.github.io/beu/',
+        cancel_url: ''
+    })
+    res.json({id: session.id});
+    
+  //   const createUser = `INSERT INTO cart
+  //       (transactionid,datecreated, customerid, productid,status, amount)
+  //     VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`;
+  //   console.log(req.body)
+  //   const values = [
+  //   req.body.transactionid,
+  //   moment(new Date()),
+  //   req.body.category,
+  //   req.body.customerid,
+  //   req.body.productid,
+  //   req.body.status,
+  //   req.body.amount,
+  //     ];
+  //   try {
+  //   const { rows } = await db.query(createUser, values);
+  //   // console.log(rows);
+  //   //  return res.status(201).send(rows);
+  //   return res.status(201).send({status:true, message: 'successful', data:rows});
+  //   } catch (error) {
+  //   return res.status(400).send(error);
+  //   }  
+  // //  },{ resource_type: "auto", public_id: `ridafycovers/${req.body.title}` })
 } else {
     res.status(405).json({
       err: `${req.method} method not allowed`
