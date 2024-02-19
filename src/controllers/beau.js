c6ff00104d094a5dbbe817eb07d1a1e4bdaa874e
nerjib <kabirnajib0@gmail.com>
2024-02-19 22:39:04 +0100

    'fixes'

--- src/controllers/beau.js
+++ src/controllers/beau.js
@@ -200,7 +200,7 @@ router.get('/admin/orderbystatus/:status', async (req, res) => {
 });
 
 router.get('/admin/wholesellers-req', async (req, res) => {
-  const getAllQ = `SELECT distinct(referenceid), customername, customerid, status, 'createdAt' from beauwholesales`;
+  const getAllQ = `SELECT distinct(referenceid), customername, customerid, status, "createdAt" from beauwholesales order by "createdAt" asc`;
   try {
     // const { rows } = qr.query(getAllQ);
     const { rows } = await db.query(getAllQ);
