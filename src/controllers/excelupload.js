57cf67403549c60ce79af528cd9cb6f31f56cf0a
nerjib <kabirnajib0@gmail.com>
2024-08-12 05:56:14 +0100

    remove lastline

--- src/controllers/excelupload.js
+++ src/controllers/excelupload.js
@@ -167,7 +167,7 @@ router.post('/customers', async (req, res) => {
             payments.push(payment);
           });
          // console.log(tutorials.shift())
-         // payments.pop();
+          payments.pop();
           Payment.bulkCreate(await(payments), { ignoreDuplicates: true, })
           .then(() => {
             res.status(200).send({
