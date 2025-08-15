6b431712550fd0bf92d070ce57232279d98b7d46
user <user@Najibs-MacBook-Pro.local>
2025-08-15 16:29:09 +0100

    pg pool config

--- src/controllers/rayconnect/config/database.js
+++ src/controllers/rayconnect/config/database.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-// const pool = new Pool(process.env.DATABASE_URL);
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+const pool = new Pool(process.env.DATABASE_URL);
 // const client = new Client({
 //   connectionString: process.env.DATATYPE === 'test' ? process.env.DATABASE_URL1 : process.env.DATABASE_URL,
 //   ssl: {
