5ba77f611ea65207005065d6aec6f9eaabd5ab7e
user <user@Najibs-MacBook-Pro.local>
2025-10-26 01:07:43 +0100

    feat: zazzau refunds

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -101,7 +101,7 @@ router.get('/', async (req, res) => {
 
   Payment.findAndCountAll({  limit, offset })
     .then(data => {
-      const sortedData = data.rows.sort((a, b) => new Date(b.period) - new Date(a.period));
+      const sortedData = data.rows.sort((a, b) => new Date(b.period).getTime() - new Date(a.period).getTime());
       const response = Helper.getPagingData({ ...data, rows: sortedData }, page, limit);
       res.send(response);
     })
